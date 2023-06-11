1ca743ad2fd32185578fab4510d3445dcbe2131d
Cassiophysics <cassio.costa1@hotmail.com>
2023-06-11 11:34:18 -0300

    alteracoes

--- Streamlit/prediction.py
+++ Streamlit/prediction.py
@@ -1,4 +1,11 @@
-import pickle
+#import joblib
+#def predict(data):
+#    xgbr2 = joblib.load('xgbr2_model.sav')
+#    return xgbr2.predict(data)
+
+
+
+import joblib
 def predict(data):
-    xgbr2 = pickle.load(open('saved_steps.pkl', 'rb'))
+    xgbr2 = joblib.load(open('saved_steps.pkl', 'rb'))
     return xgbr2.predict(data)
