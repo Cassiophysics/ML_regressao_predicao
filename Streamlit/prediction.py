98435714373ec4a9e20a495bd8223f4865ae695c
Cassiophysics <cassio.costa1@hotmail.com>
2023-06-12 07:18:45 -0300

    commit

--- Streamlit/prediction.py
+++ Streamlit/prediction.py
@@ -3,6 +3,11 @@ def predict(data):
     xgbr2 = joblib.load('xgbr2_model.sav')
     return xgbr2.predict(data)
 
+import streamlit as st
+import os
+path_to_find = os.listdir()
+st.title(path_to_find)
+
 #import joblib
 #def predict(data):
 #    xgbr2 = joblib.load(open('saved_steps.pkl', 'rb'))
